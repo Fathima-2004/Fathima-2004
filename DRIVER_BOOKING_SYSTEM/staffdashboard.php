ee382ca77c7963dccc7457e08ab233b64e899dd5
fathima-2004 <ksfathima083@gmail.com>
2024-10-21 06:21:29 +0530

    Add files via upload

---
+++ DRIVER_BOOKING_SYSTEM/staffdashboard.php
@@ -0,0 +1,33 @@
+<?php
+include("./auth/staffauth.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="staffhome.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="nanbar">
+        <div class="navbar">
+            <a class="hdng"  href="">DRIVER BOOKING SYSTEM</a>
+            <div class="link">
+                <a href="managebookings.php">Manage bookings</a>
+                <a href="driverprofile.php">Edit Profile</a>
+                <a href="logout.php">Logout</a>
+            </div>
+        </div>
+        </div>
+    </nav>
+
+      <div class="doc">
+        <h1 class="hg">staff Dashboard</h1>
+      </div>
+
+</body>
+
+</html>
