d47fd5db97f9d7c869706fec52ea0437282a237e
fathima-2004 <ksfathima083@gmail.com>
2024-12-06 11:44:06 +0530

    Delete DRIVER_BOOKING_SYSTEM directory

--- DRIVER_BOOKING_SYSTEM/auth/staffauth.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    ob_start();
-    if (!isset($_SESSION["staffauth"]) ||  $_SESSION["staffauth"] !== true) {
-        header("Location: ./home.html");
-        exit();
-    }
-    ob_end_flush();
-?>
